715c9624523eab85b04c9b67d702e1d64e6cedcd
yurizzxz <yuri.alves.silva@gmail.com>
2025-02-19 20:34:09 -0300

    feat: images e finalização da home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,33 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import { Montserrat, Oxanium } from "next/font/google";
+import type { Metadata } from 'next'
+import './globals.css'
+import { Oxanium } from 'next/font/google'
 
 export const metadata: Metadata = {
-  title: "Dev Code",
-};
+	title: 'Dev Code',
+}
 
 const oxanium = Oxanium({
-  weight: ["500", "600"],
-  subsets: ["latin"],
-  variable: "--font-oxanium",
-});
+	weight: ['500', '600'],
+	subsets: ['latin'],
+	variable: '--font-oxanium',
+})
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="en" className={`${oxanium.variable}`}>
-      <head>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
-        />
-      </head>
-      <body className="bg-gray-900">{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en" className={`${oxanium.variable}`}>
+			<head>
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
+				/>
+			</head>
+			<body className="bg-[#0f1216] text-gray-100 bg-[url('/background.png')] bg-no-repeat bg-top sm:bg-right-top">
+				{children}
+			</body>
+		</html>
+	)
 }
